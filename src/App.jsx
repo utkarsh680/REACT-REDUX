5ee2be08345b9c215f11faf24cb172409581bd4d
Utkarsh Singh <singhrishu680@gmail.com>
2023-08-02 14:34:03 +0530

    Add action reducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import './App.css'
 const App = () => {
   return (
     <>
-    <h1>Hello</h1>
+    <div className="container">
+      <h1>Increment/Decrement counter</h1>
+      <h4>Using React and Redux</h4>
+      <div className="quantity">
+        <a className='quantityMinus' title = "Decrement"><span>-</span></a>
+        <input type="text" name='quantity' className='quantityInput' value='0' />
+        <a className='quantityPlus' title = "Increment"><span>+</span></a>
+      </div>
+
+    </div>
     </>
   )
 }
